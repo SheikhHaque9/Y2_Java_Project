368146f5e5eb7c2a0846b975d5120ba1fe62af87
SheikhHaque9 <148403264+SheikhHaque9@users.noreply.github.com>
2024-06-04 15:06:37 +0100

    Add files via upload

---
+++ Y2_Project/lmb_test/src/lmb_test/the_test.java
@@ -0,0 +1,17 @@
+package lmb_test;
+
+import lombok.Getter;
+
+@Getter
+public class the_test {
+
+	public static void main(String[] args) 
+	{
+		System.out.println();
+	}
+	
+	private String name;
+	private float weight;
+	private int age;
+
+}
